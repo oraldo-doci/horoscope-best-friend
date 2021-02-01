be015a9263350d345332b7dbfebc7952dfa1ac96
Oraldo <72758131+oraldo-doci@users.noreply.github.com>
2021-02-01 01:06:16 +0100

    Add files via upload
    first commit

---
+++ zodiac-dog/index.js
@@ -0,0 +1,100 @@
+function myFunction() {
+  var x = document.getElementById("card-shepard");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionG() {
+  var x = document.getElementById("card-golden");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionW() {
+  var x = document.getElementById("card-welsh-corgi");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionP() {
+  var x = document.getElementById("card-pomeranian");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionR() {
+  var x = document.getElementById("card-rottweiler");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionCh() {
+  var x = document.getElementById("card-chow-cow");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionGe() {
+  var x = document.getElementById("card-greyhound");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunctionTr() {
+  var x = document.getElementById("card-terrier");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionAs() {
+  var x = document.getElementById("card-australian");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionSb() {
+  var x = document.getElementById("card-St-Benard");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionCa() {
+  var x = document.getElementById("card-Cavalier-King-Charles-spaniel");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunctionPo() {
+  var x = document.getElementById("card-poodle");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
